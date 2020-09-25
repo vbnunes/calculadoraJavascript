d6af1c8d7adb7ad8af2b193d2bcbc3e297eb8a65
Marcos Madeira Marcio <madeiramarcos94@gmail.com>
2020-09-24 21:09:27 -0300

    Created initial display and 5 operations for the calculator

--- index.js
+++ index.js
@@ -1,31 +1,77 @@
-const readline = require('readline');
+const readline = require('readline-sync');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+while (true) {
+  displayInfo()
+  let option = readline.question('Option: ');
 
+  console.log(`Selected operation: ${option}`);
 
-var recursiveAsyncReadLine = () => {
-  rl.question("Selecione o que vai ser feito:\n"
-    + "1) Adição\n"
-    + "2) Subtração\n"
-    + "3) Exit\n"
-    + "Opção: "
-    , function (opcao) {
+  
+// validando se input é número
+  if (isNaN(option)) {   
+    console.error('please input a number');
+    continue;
+  }
+  
+// opções com apenas 1 input têm função diferente
+  if (option === '5') {
+    var value = sqrtInput(); 
+  }
 
-      switch (opcao) {
-        case "1":
-          console.log("Resultado da adição: ");
-          break;
-        case "3":
-          rl.close();
-          break;
-        default:
-          console.log("Opção inválida\n");
-      }
-      recursiveAsyncReadLine();
-    });
-};
+  else {
+    var values = getInput();
+  }
 
-recursiveAsyncReadLine();
+  switch(option) {
+    case '1': 
+      let addition = parseFloat(values[0]) + parseFloat(values[1]);
+      console.log(`Result: ${addition}`);
+    break;
+
+    case '2':
+      let subtraction = parseFloat(values[0]) - parseFloat(values[1]);
+      console.log(`Result: ${subtraction}`);
+    break;
+
+    case '3':
+      let multiplication = parseFloat(values[0]) * parseFloat(values[1]);
+      console.log(`Result: ${multiplication}`);
+    break;
+
+    case '4':
+      let division = parseFloat(values[0]) / parseFloat(values[1]);
+      console.log(`Result: ${division}`);
+    break;
+
+    case '5':
+      let sqrt = Math.sqrt(value);
+      console.log(`Result: ${sqrt}`);
+    break;
+
+    
+    default:
+      console.error('Please select a valid operation');
+  }
+}
+
+function getInputs() {
+  let value1 = readline.questionFloat('value 1: \n');
+  let value2 = readline.questionFloat('value 2: \n');
+  
+  return [value1, value2];
+}
+
+function sqrtInput() {
+  let sqrtValue = readline.questionFloat('sqrt value: \n');
+  return sqrtValue;
+}
+
+function displayInfo() {
+  console.log('Select an operation:')
+  console.log('1. Addition');
+  console.log('2. Subtraction');
+  console.log('3. Multiplication');
+  console.log('4. Division');
+  console.log('5. Square root');
+
+}
